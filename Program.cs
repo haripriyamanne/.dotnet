98429332b74470d0ce203d58ca14d1499d8ab384
Haripriya <haripriyamanne4@gmail.com>
2021-04-02 15:31:57 +0530

    This is version

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace myfirstapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Date and Time is:" + DateTime.Now);
+        }
+
+
+        
+
+    }
+}
